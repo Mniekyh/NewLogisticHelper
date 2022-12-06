f8339bce5e457ad87991c536ac925af11ae527be
lukaszszsz <82957112+lukaszszsz@users.noreply.github.com>
2022-12-06 18:30:09 +0100

    lukasz_gut

---
+++ Logistic-Helper-main/LogisticHelper.Models/Account.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using System.Web;
+
+
+namespace LogisticHelper.Models
+{
+    public class AccountController
+    {
+        public static int userId;
+
+        public string Name { get; set; }
+        public string Password { get; set; }
+        public string Email { get; set; }
+
+
+
+        public string user_id { get; set; }
+        public string miejscowosc { get; set; }
+        public string ulica { get; set; }
+
+
+        public string? txtCon { get; set; }
+        public string? txtCity { get; set; }
+
+        public string? con { get; set; }
+        public string? city { get; set; }
+        public string? com { get; set; }
+
+    }
+}
